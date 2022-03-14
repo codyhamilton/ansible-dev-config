b541623cdd276b4d5968bc9b0b408f10c36ce36d
William Hamilton <codyh@ctzns.net>
2022-03-14 15:55:04 -0500

    feat(core): throw an error if ansible-playbook not found

--- configure.sh
+++ configure.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ansible-playbook playbook.yml -i inventory
-source ~/.bash_profile
+if command -v 'ansible-playbook'; then
+	ansible-playbook playbook.yml -i inventory
+	source ~/.bash_profile
+else
+	echo "You need to install ansible first"
+	exit 1
+fi
